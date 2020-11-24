6554693d0baa835989593f522181a8b5b91d1f07
Pham Hong Vinh <phamvinh257@gmail.com>
2020-11-24 23:40:38 +0700

    First commit

---
+++ ColorTransformer.h
@@ -0,0 +1,157 @@
+﻿#pragma once
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+
+class ColorTransformer
+{
+public:
+	/*
+	Hàm nhận vào một ảnh, thay đổi độ sáng của ảnh này và lưu kết quả vào ảnh mới
+	Tham so:
+		sourceImage	: ảnh ban đầu
+		destinationImage: ảnh kết quả
+		b	: giá trị số nguyên dùng để thay đổi độ sáng của ảnh
+	Hàm trả về:
+		1: Nếu thành công thì trả về ảnh kết quả (ảnh gốc vẫn giữ nguyên giá trị)
+		0: Nếu không tạo được ảnh kết quả hoặc ảnh input không tồn tại
+	*/
+	int ChangeBrighness(const cv::Mat& sourceImage, cv::Mat& destinationImage, short b) 
+	{
+
+		if (sourceImage.channels() == 3)  // rgb image
+			destinationImage = cv::Mat(sourceImage.rows, sourceImage.cols, CV_8UC3);
+		else if (sourceImage.channels() == 1)
+			destinationImage = cv::Mat(sourceImage.rows, sourceImage.cols, CV_8UC1);
+		else
+			return 0;
+
+		//truy xuất đến tọa độ ảnh (i,j)
+		//image.at<uchar>(j, i) = 255;
+		for (int i = 0; i < sourceImage.rows; i++)
+		{
+			const uchar* rowOrgData = sourceImage.ptr<uchar>(i);
+			uchar* rowDstData = destinationImage.ptr<uchar>(i);
+
+			for (int j = 0; j < sourceImage.cols * sourceImage.channels(); j++)
+			{
+				if (rowOrgData[j] + b <= 255 && rowOrgData[j] + b >= 0)
+					rowDstData[j] = rowOrgData[j] + b;
+				else
+				{
+					if (rowOrgData[j] + b > 255)
+						rowDstData[j] = 255;
+					if (rowOrgData[j] + b < 0)
+						rowDstData[j] = 0;
+				}
+
+			}
+		}
+
+		return 1;
+	}
+
+	/*
+	Hàm nhận vào một ảnh, thay đổi độ tương phản của ảnh này và lưu kết quả vào ảnh mới
+	Tham so :
+		sourceImage : ảnh ban đầu
+		destinationImage : ảnh kết quả
+		c	: giá trị số thực dùng để thay đổi độ tương phản của ảnh
+	Hàm trả về:
+		1: Nếu thành công thì trả về ảnh kết quả (ảnh gốc vẫn giữ nguyên giá trị)
+		0: Nếu không tạo được ảnh kết quả hoặc ảnh input không tồn tại
+	*/	
+	int ChangeContrast(const cv::Mat& sourceImage, cv::Mat& destinationImage, float c) 
+	{
+
+
+		if (sourceImage.channels() == 3)  // rgb image
+			destinationImage = cv::Mat(sourceImage.rows, sourceImage.cols, CV_8UC3);
+		else if (sourceImage.channels() == 1)
+			destinationImage = cv::Mat(sourceImage.rows, sourceImage.cols, CV_8UC1);
+		else
+			return 0;
+
+		//truy xuất đến tọa độ ảnh (i,j)
+		//image.at<uchar>(j, i) = 255;
+		for (int i = 0; i < sourceImage.rows; i++)
+		{
+			const uchar* rowOrgData = sourceImage.ptr<uchar>(i);
+			uchar* rowDstData = destinationImage.ptr<uchar>(i);
+
+			for (int j = 0; j < sourceImage.cols * sourceImage.channels(); j++)
+			{
+				if (rowOrgData[j] * c <= 255 && rowOrgData[j] * c >= 0)
+					rowDstData[j] = rowOrgData[j] * c;
+				else
+				{
+					if (rowOrgData[j] * c > 255)
+						rowDstData[j] = 255;
+					if (rowOrgData[j] * c < 0)
+						rowDstData[j] = 0;
+				}
+
+			}
+		}
+
+		return 1;
+	}
+	
+	
+	/*
+	Hàm tính lược đồ màu tổng quát cho ảnh bất kỳ
+	Tham so :
+		sourceImage : ảnh ban đầu
+		histMatrix : ma trận histogram (nChannels x 256), mỗi dòng là 1 mảng 256 phần tử histogram của từng kênh màu
+	Hàm trả về:
+		1: Nếu thành công thì trả về matrix kết quả (ảnh gốc vẫn giữ nguyên giá trị)
+		0: Nếu không tính được histogram hoặc ảnh input không tồn tại
+	*/
+	int CalcHistogram(const cv::Mat& sourceImage, cv::Mat& histMatrix);
+	
+	/*
+	Hàm cân bằng lược đồ màu tổng quát cho ảnh bất kỳ
+	Tham so :
+		image : ảnh dùng để tính histogram
+		histImage : ảnh histogram
+	Hàm trả về:
+		1: Nếu thành công vẽ được histogram
+		0: Nếu không vẽ được histogram
+	*/
+	int HistogramEqualization(const cv::Mat& sourceImage, cv::Mat& destinationImage);
+	
+
+
+		/*
+	Hàm cân bằng lược đồ màu tổng quát cho ảnh bất kỳ
+	Tham so :
+		histMatrix : ma trận histogram đã tính được
+		histImage : ảnh histogram được vẽ
+	Hàm trả về:
+		1: Nếu thành công vẽ được histogram
+		0: Nếu không vẽ được histogram
+	*/
+	int DrawHistogram(const cv::Mat& histMatrix, cv::Mat& histImage);
+
+	
+	/*
+	Hàm so sánh hai ảnh
+	Tham so :
+		image1 : ảnh thứ nhất
+		image2 : ảnh thứ hai
+	Hàm trả về:
+		độ đo sự tương đồng giữa hai ảnh
+	*/
+	float CompareImage(const cv::Mat& image1, cv::Mat& image2);
+	
+	
+	ColorTransformer() 
+	{
+
+	}
+
+	~ColorTransformer() 
+	{
+
+	}
+};
+
---
+++ Converter.h
@@ -0,0 +1,212 @@
+﻿#pragma once
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+
+#define R 2
+#define G 1
+#define B 0
+#define H 0
+#define S 1
+#define V 2
+
+
+class Converter
+{
+	/*
+	Hàm chuyển đổi không gian màu của ảnh từ RGB sang GrayScale
+	sourceImage: ảnh input
+	destinationImage: ảnh output	
+	Hàm trả về
+	0: nếu chuyển thành công
+	1: nếu chuyển thất bại (không đọc được ảnh input,...)
+	*/
+	int RGB2GrayScale(const cv::Mat& sourceImage, cv::Mat& destinationImage) 
+	{
+		if (sourceImage.channels() != 3)
+			return 1;
+
+		destinationImage = cv::Mat(sourceImage.rows, sourceImage.cols, CV_8UC1);
+
+		for (int row = 0; row < sourceImage.rows; row++) 
+		{
+			for (int col = 0; col < sourceImage.cols; col++) 
+			{
+				cv::Vec3b currentPixel = sourceImage.at<cv::Vec3b>(row, col);
+				destinationImage.at<uchar>(row, col) = 0.299*currentPixel[R] + 0.587*currentPixel[G] + 0.114*currentPixel[B];
+			}
+		}
+		return 0;
+	}
+
+	/*
+	Hàm chuyển đổi không gian màu của ảnh từ GrayScale sang RGB
+	sourceImage: ảnh input
+	destinationImage: ảnh output
+	Hàm trả về
+	0: nếu chuyển thành công
+	1: nếu chuyển thất bại (không đọc được ảnh input,...)
+	*/
+	int GrayScale2RGB(const cv::Mat& sourceImage, cv::Mat& destinationImage) 
+	{
+		if (sourceImage.channels() != 1)
+			return 1;
+		destinationImage = cv::Mat(sourceImage.rows, sourceImage.cols, CV_8UC3);
+
+		for (int row = 0; row < sourceImage.rows; row++) 
+		{
+			for (int col = 0; col < sourceImage.cols; col++) 
+			{
+				uchar currentPixel = sourceImage.at<uchar>(row, col);
+				destinationImage.at<cv::Vec3b>(row, col)[R] = currentPixel;
+				destinationImage.at<cv::Vec3b>(row, col)[G] = currentPixel;
+				destinationImage.at<cv::Vec3b>(row, col)[B] = currentPixel;
+			}
+		}
+
+		return 0;
+	}
+	
+	double findMax(double r, double g, double b) 
+	{
+		double firstMax = (r > g ? r : g);
+		return (firstMax > b ? firstMax : b);
+	}
+
+	double findMin(double r, double g, double b) 
+	{
+		double firstMin = (r > g ? g : r);
+		return (firstMin > b ? b : firstMin);
+	}
+
+	/*
+	Hàm chuyển đổi không gian màu của ảnh từ RGB sang HSV
+	sourceImage: ảnh input
+	destinationImage: ảnh output
+	Hàm trả về
+	0: nếu chuyển thành công
+	1: nếu chuyển thất bại (không đọc được ảnh input,...)
+	*/
+	int RGB2HSV(const cv::Mat& sourceImage, cv::Mat& destinationImage) 
+	{
+		if (sourceImage.channels() != 3)
+			return 1;
+
+		destinationImage = cv::Mat(sourceImage.rows, sourceImage.cols, CV_8UC3);
+
+		for (int row = 0; row < sourceImage.rows; row++) 
+		{
+			for (int col = 0; col < sourceImage.cols; col++) 
+			{
+				cv::Vec3b currentPixel = sourceImage.at<cv::Vec3b>(row, col);
+				double h, s, v;
+
+				double r_scaled = currentPixel[R] / 255.0;
+				double g_scaled = currentPixel[G] / 255.0;
+				double b_scaled = currentPixel[B] / 255.0;
+
+				double val_max = findMax(r_scaled, g_scaled, b_scaled);
+				double val_min = findMin(r_scaled, g_scaled, b_scaled);
+				double delta = val_max - val_min;
+
+				v = val_max * 255;
+				if (delta < 0.00001) 
+				{
+					h = 0.0;
+					s = NAN;
+				}
+				else if (val_max <= 0.0) 
+				{
+					s = 0.0;
+					h = NAN;
+				}
+				else 
+				{
+					s = delta / val_max;
+					if (r_scaled >= val_max) {
+						h = (g_scaled - b_scaled) / delta;
+					}
+					else if (g_scaled >= val_max) {
+						h = 2.0 + (b_scaled - r_scaled) / delta;
+					}
+					else {
+						h = 4.0 + (r_scaled - g_scaled) / delta;
+					}
+					h *= 60.0;
+					if (h < 0.0)
+						h += 360;
+
+					//Turn to OpenCV style
+					s *= 255.0;
+					h /= 2.0;
+				}
+
+				destinationImage.at<cv::Vec3b>(row, col)[H] = (uchar)h;
+				destinationImage.at<cv::Vec3b>(row, col)[S] = (uchar)s;
+				destinationImage.at<cv::Vec3b>(row, col)[V] = (uchar)v;
+
+			}
+		}
+
+		return 0;
+	}
+	
+	
+	
+	/*
+	Hàm chuyển đổi không gian màu của ảnh từ HSV sang RGB
+	sourceImage: ảnh input
+	destinationImage: ảnh output
+	Hàm trả về
+	0: nếu chuyển thành công
+	1: nếu chuyển thất bại (không đọc được ảnh input,...)
+	*/
+	int HSV2RGB(const cv::Mat& sourceImage, cv::Mat& destinationImage);
+	
+	
+	
+public:
+	/*
+	Hàm chuyển đổi không gian màu của ảnh
+	sourceImage: ảnh input
+	destinationImage: ảnh output cùng kích thước, cùng loại với ảnh input
+	type: loại chuyển đổi
+		0,1: chuyển từ RGB sang GrayScale và ngược lại
+		2,3: chuyển từ RGB sang HSV và ngược lại		
+	Hàm trả về
+		0: nếu chuyển thành công
+		1: nếu chuyển thất bại (không đọc được ảnh input hay type không chính xác,...)
+	*/
+	int Convert(cv::Mat& sourceImage, cv::Mat& destinationImage, int type) 
+	{
+		switch (type)
+		{
+		case 0:
+			return RGB2GrayScale(sourceImage, destinationImage);
+		case 1:
+			return GrayScale2RGB(sourceImage, destinationImage);
+		case 2:
+			return RGB2HSV(sourceImage, destinationImage);
+		case 3:
+			return HSV2RGB(sourceImage, destinationImage);
+		default:
+			return 1;
+		}
+	}
+
+	int Convert(IplImage* sourceImage, IplImage* destinationImage, int type) 
+	{
+		cv::Mat org = cv::cvarrToMat(sourceImage);
+		cv::Mat dst = cv::cvarrToMat(destinationImage);
+		return Convert(org, dst, type);
+	}
+
+	Converter() 
+	{
+
+	}
+	~Converter() 
+	{
+
+	}
+};
+
